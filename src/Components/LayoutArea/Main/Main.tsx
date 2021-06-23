a53c0c3a0ed4bea31d73d462464c05048082a6c9
Kobi Shasha <kobi.shasha@gmail.com>
2021-06-23 03:10:51 +0300

    CSS Improvments

--- src/Components/LayoutArea/Main/Main.tsx
+++ src/Components/LayoutArea/Main/Main.tsx
@@ -3,7 +3,10 @@ import "./Main.css";
 function Main(): JSX.Element {
     return (
         <div className="Main">
-			<img id="leo_img" src="https://media.giphy.com/media/TjAcxImn74uoDYVxFl/giphy.gif"/>
+            <h1>Best Player in the world</h1>
+           
+            <img src="https://media.giphy.com/media/TjAcxImn74uoDYVxFl/giphy.gif"/>
+           
         </div>
     );
 }
